a45763d585485cb072d6963a0642da016ad3dd4b
PREMSAGAR <126579368+premsagar39@users.noreply.github.com>
2026-01-09 13:11:20 +0530

    Update 01_basic_queries.sql

--- queries/01_basic_queries.sql
+++ queries/01_basic_queries.sql
@@ -1,1 +1,16 @@
+#all columns
+select * from superstore;
+
+#how many columns
+select count(*) from superstore;
+
+#Find customers from selected regions
+SELECT DISTINCT CustomerName,region
+FROM superstore
+WHERE region IN ('West', 'East');
+
+#Customers NOT from selected regions
+SELECT customername, region
+FROM superstore
+WHERE region NOT IN ('West','East');
 
