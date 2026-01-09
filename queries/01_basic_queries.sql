068056fa8441246b337d87e0fdfab76c64285781
PREMSAGAR <126579368+premsagar39@users.noreply.github.com>
2026-01-09 17:32:37 +0530

    Add various sales-related queries to SQL file

--- queries/01_basic_queries.sql
+++ queries/01_basic_queries.sql
@@ -14,3 +14,21 @@ SELECT customername, region
 FROM superstore
 WHERE region NOT IN ('West','East');
 
+#cost estimation sales-profit
+select orderid,sales,Profit,(sales-Profit)
+as cost_estimation from superstore where Profit>0 and sales>100;
+
+#orders with sales between 500 and 1000
+select OrderID,sales from superstore where sales between 500 and 1000;
+
+#Top profitable orders 
+SELECT orderid, sales, profit
+FROM superstore
+ORDER BY profit DESC;
+
+#orders with sales higher than all office suppliers sales
+select OrderID,sales from superstore where 
+sales > all (select sales from superstore where productcategory = officesuppliers );
+
+#customer with above average sales
+select CustomerName,sales from superstore where sales > (select avg(sales) from superstore);
